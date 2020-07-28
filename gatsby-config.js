128d2683fd4f5c9bed51f95612ee4c046115483c
Simon Stratton <simon.stratton@safebear.co.uk>
2020-07-28 21:24:51 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // edit below
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Novela by Narative`,
